refactoring, added a normalization technic (not on github for confidentiality), fixed the main production, and little cleanings

core/BalancedImageDataGenerator.py
@@ -7,6 +7,7 @@ from tools.MaskCreator import MaskCreator
 from sklearn.utils import shuffle
 import random
 import numpy as np
+from core.ColorSpaceConverter import ColorSpaceConverter
 
 
 class BalancedImageDataGenerator(ImageDataGenerator):
@@ -169,9 +170,12 @@ class BalancedImageDataGenerator(ImageDataGenerator):
                 else:
                     lbl = self.resize_if_needed(lbl, is_label=True)
 
+                FileManager.SaveImage(lbl, name, "outputs/gen_lbl")
+
                 if binarized:
                     lbl = self.binarylab(lbl[:, :, 0], self.width, self.height, self.nblbl)
                     lbl = np.reshape(lbl, (self.width * self.height, self.nblbl))
+
                 if self.yield_names:
                     yield lbl, name
                 else:
@@ -187,7 +191,6 @@ class BalancedImageDataGenerator(ImageDataGenerator):
         while True:
             i = 0
             for a in self.angles:
-
                 name = self.current_iteration_img[i % length]
                 img = FileManager.LoadImage(name, self.src_directory)
 
@@ -201,8 +204,8 @@ class BalancedImageDataGenerator(ImageDataGenerator):
 
                 if self.norm_type == NormType.Equalize:
                     img = self.normalize(img)
-                elif self.norm_type == NormType.StdMean:
-                    img = self.normalize_std(img)
+                elif self.norm_type & NormType.EqualizeClahe == NormType.EqualizeClahe:
+                    img = self.normalize_clahe(img)
 
                 if self.allow_transform:
                     random.shuffle(self.transforms_family)
@@ -211,9 +214,15 @@ class BalancedImageDataGenerator(ImageDataGenerator):
 
                 if self.magentize and not self.torify:
                     img = self.colorize_void(idx, color, img)
+
+                if self.norm_type & NormType.StdMean == NormType.StdMean:
+                    img = self.normalize_std(img)
+                elif self.norm_type & NormType.SPHcl == NormType.SPHcl:
+                    img = ColorSpaceConverter.get_spherical_hcl(img)
+
                 j += 1
 
-                #FileManager.SaveImage(img, name, "outputs/gen_img")
+                FileManager.SaveImage(img, name, "outputs/gen_img")
                 img = np.rollaxis(img, 2) if roll_axis else img
                 if self.yield_names:
                     yield img, name

core/DatasetManager.py
@@ -102,13 +102,39 @@ class DatasetManager:
 
         files = [f for f in os.listdir(self.labels_path) if re.match(self.reg, f.lower())]
 
-        nbVoid = cv2.countNonZero(cv2.bitwise_not(self.mask))
+        void_mask = cv2.bitwise_not(self.mask)
+        nbVoid = cv2.countNonZero(void_mask)
 
         for f in files:
             imgSrc = FileManager.LoadImage(f, self.labels_path)
             b, g, r = cv2.split(imgSrc)
 
             annots = np.zeros(b.shape, np.uint8)
+
+            ok = False
+            i = 0
+            ker = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+            void_mask_ex = cv2.dilate(void_mask, ker)
+            while not ok:
+                bprime = cv2.dilate(b, ker)
+                gprime = cv2.dilate(g, ker)
+                rprime = cv2.dilate(r, ker)
+
+                b = cv2.bitwise_or(cv2.bitwise_and(bprime, void_mask_ex), b)
+                g = cv2.bitwise_or(cv2.bitwise_and(gprime, void_mask_ex), g)
+                r = cv2.bitwise_or(cv2.bitwise_and(rprime, void_mask_ex), r)
+
+                extended = cv2.bitwise_or(cv2.bitwise_or(b, g), r)
+                extended = cv2.morphologyEx(extended, cv2.MORPH_CLOSE, ker)
+                ok = abs(cv2.countNonZero(cv2.bitwise_and(extended, self.mask)) - cv2.countNonZero(self.mask)) < 5
+                i += 1
+                if i > 10:
+                    print "No convergence found while filling holes between void and classes in image %s, please correct the image before continue" % f
+                    cv2.imshow(f, extended)
+                    cv2.imshow("b", b)
+                    cv2.waitKey(0)
+                    return
+
 
             idxSky = b > 0
             idxVegetation = g > 0
@@ -117,7 +143,7 @@ class DatasetManager:
             annots[idxSky] = Classes.SKY
             annots[idxVegetation] = Classes.VEGETATION
             annots[idxBuild] = Classes.BUILT
-            annots[self.mask == 0] = Classes.VOID
+            annots[void_mask > 0] = Classes.VOID
 
             self.classes_weigth[Classes.SKY] += cv2.countNonZero(b)
             self.classes_weigth[Classes.VEGETATION] += cv2.countNonZero(g)
@@ -243,8 +269,6 @@ class DatasetManager:
             nb = idx_sky.sum() / float(tot)
             ng = idx_veg.sum() / float(tot)
             nr = idx_build.sum() / float(tot)
-
-            # print "b=%1.2f, g=%1.2f, r = %1.2f" % (nb, ng, nr)
 
             outputs_tables = {0: output_sky_most, 1: output_veg_most, 2: output_build_most}
 

core/ImageDataGenerator.py
@@ -7,12 +7,15 @@ import random
 import numpy as np
 from enum import IntEnum
 from tools.ImageTransform import ImageTransform
+from core.ColorSpaceConverter import ColorSpaceConverter
 
 
 class NormType(IntEnum):
-    Equalize = 0
-    StdMean = 2
-    Nothing = 1
+    Equalize = 1
+    EqualizeClahe = 2
+    StdMean = 4
+    SPHcl = 8
+    Nothing = 16
 
 
 class PossibleTransform(IntEnum):
@@ -21,7 +24,8 @@ class PossibleTransform(IntEnum):
     GaussianNoise = 2
     Sharpen = 3
     AddSub = 5
-    Invert = 6
+    AddSubChannel = 7
+    Invert = 7
 
 
 class TransformDescriptor():
@@ -65,7 +69,7 @@ class ImageDataGenerator:
         self.rotate = rotate
         self.batch_size = batch_size
         self.seed = seed
-        self.mask = self.get_void_mask(input_width, input_height)
+        self.mask = self.get_void_mask(target_width, target_height)
         self.reg = r'\w+\.(jpg|jpeg|png)'
         self.img_files = sorted([f for f in os.listdir(self.src_directory) if re.match(self.reg, f.lower())])
         self.lbl_files = sorted([f for f in os.listdir(self.labels_directory) if re.match(self.reg, f.lower())])
@@ -115,10 +119,10 @@ class ImageDataGenerator:
                 else:
                     img = self.resize_if_needed(img)
 
-                if self.norm_type == NormType.Equalize:
+                if self.norm_type & NormType.Equalize == NormType.Equalize:
                     img = self.normalize(img)
-                elif self.norm_type == NormType.StdMean:
-                    img = self.normalize_std(img)
+                elif self.norm_type & NormType.EqualizeClahe == NormType.EqualizeClahe:
+                    img = self.normalize_clahe(img)
 
                 if self.allow_transform:
                     random.shuffle(self.transforms_family)
@@ -127,8 +131,13 @@ class ImageDataGenerator:
 
                 if self.magentize and not self.torify:
                     img = self.colorize_void(idx, color, img)
-                j += 1
 
+                if self.norm_type & NormType.StdMean == NormType.StdMean:
+                    img = self.normalize_std(img)
+                elif self.norm_type & NormType.SPHcl == NormType.SPHcl:
+                    img = ColorSpaceConverter.get_spherical_hcl(img)
+
+                j += 1
                 img = np.rollaxis(img, 2) if roll_axis else img
                 if self.yield_names:
                     yield img, name
@@ -190,8 +199,8 @@ class ImageDataGenerator:
                 batch = []
                 i = 0
 
-    def normalize(self, rgb):
-        b, g, r = cv2.split(rgb)
+    def normalize_clahe(self, bgr):
+        b, g, r = cv2.split(bgr)
 
         clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(int(self.width / 30), int(self.height / 30)))
         channels = (b, g, r)
@@ -229,6 +238,17 @@ class ImageDataGenerator:
         return cv2.merge((b, g, r))
 
     @staticmethod
+    def normalize(bgr):
+        b, g, r = cv2.split(bgr)
+
+        channels = (b, g, r)
+        equalized = []
+        for c in channels:
+            equalized.append(cv2.equalizeHist(c))
+
+        return cv2.merge(equalized)
+
+    @staticmethod
     def normalize_std(rgb):
         b, g, r = cv2.split(rgb)
 
@@ -264,6 +284,16 @@ class ImageDataGenerator:
         return res
 
     @staticmethod
+    def add_or_sub_per_channel(image):
+        b, g, r = cv2.split(image)
+        channels = [b, g, r]
+
+        a_o_s = []
+        for c in channels:
+            a_o_s.append(ImageDataGenerator.add_or_sub(c))
+        return cv2.merge(a_o_s)
+
+    @staticmethod
     def add_or_sub(image):
         bound = 50
         add_val = random.randint(0, bound) - int(bound/2)
@@ -280,7 +310,7 @@ class ImageDataGenerator:
 
     @staticmethod
     def multiply(image):
-        bound = 0.15
+        bound = 0.20
         image = image.astype(np.float64)
         factor = random.uniform(0.0, bound) - bound/2
         image *= (1 - factor)